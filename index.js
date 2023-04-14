e9c65a35e88f8fe9f6dedf70d5080531f89c191b
Gilson <115327502+gil779023@users.noreply.github.com>
2023-04-13 22:12:27 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+const apiKey = '6cdf5d65';
+const frmPesquisa = document.querySelector("form");
+
+frmPesquisa.onsubmit = (ev) => {
+    ev.preventDefault();
+    
+    const pesquisa = ev.target.pesquisa.value;
+
+
+    if (pesquisa == ""){
+        alert("Preencha o campo");
+        return;
+    }
+
+    fetch(`http://www.omdbapi.com/?s=${pesquisa}&apikey=${apiKey}`)
+        .then(result => result.json())
+        .then(json => carregaLista(json));
+
+}
+
+
+
+const  carregaLista = (json) => {
+    const lista = document.querySelector("div.lista");
+    lista.innerHTML = "";
+
+    if (!json.Response){
+        alert("Nenhum Filme Encontrado");
+        return
+    }
+
+    json.Search.forEach(element => {
+        
+
+        let item = document.createElement("div");
+        item.classList.add("item");
+
+        item.innerHTML = `<img src="${element.Poster}"><h2>${element.Title}</h2>`;
+        lista.appendChild(item);
+    });
+} 
